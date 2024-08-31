d796fe9cf323e4c1b55f110c33cd58790e5aac80
reph0t <138173311+reph0t@users.noreply.github.com>
2024-08-31 15:27:20 -0400

    Update main.py

--- Basic List Operations/main.py
+++ Basic List Operations/main.py
@@ -1,26 +1,27 @@
-def total(numbers):
+def total(numbers):    # Calculates the sum  of the list 
     print(f"Total: {sum(numbers)}")
 
-def average(numbers):
+def average(numbers):    # Calculates the average of the list 
     print(f"Average: {sum(numbers) / len(numbers)}")
 
-def small_large(numbers):
+def small_large(numbers):    # Prints the smallest and largest number in the list 
     print(f"Smallest Number: {min(numbers)}")
     print(f"Largest Number: {max(numbers)}")
 
 def main():
-    user_input = input("Enter a list of numbers separated by spaces: ").split()
+    user_input = input("Enter a list of numbers separated by spaces: ").split()    # Asks the user for input 
     numbers = [int(num) for num in user_input] # Convert each input to an integer
 
+    # a new list is initiliazed 
     new_list = []
 
-    for i in numbers:
-        if i not in new_list:
-            new_list.append(i)
+    for i in numbers: # iterates in the old list
+        if i not in new_list: # if the number is not in the new_list
+            new_list.append(i)     # add the number in that list 
 
-    print(new_list)
-
-    total(new_list)
+    print(new_list) 
+    
+    total(new_list) 
     average(new_list)
     small_large(new_list)
 
