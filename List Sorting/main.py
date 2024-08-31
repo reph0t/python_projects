5de9ae4a4da8a536c1a4bb6ff669010d72172b16
reph0t <138173311+reph0t@users.noreply.github.com>
2024-08-31 16:27:54 -0400

    Update main.py

--- List Sorting/main.py
+++ List Sorting/main.py
@@ -1,33 +1,34 @@
 def ascend_sort(array):
+    n = len(array)
 
-    # loop to access each array element
-    for i in range(len(array)):
-        # loop to compare array elements
-        for j in range(0, len(array) - i - 1):
+    for i in range(n):
+        already_sorted = True
 
-            # compare two adjacent elements
-            # change > to < to sort in descending order
-            if array[j] > array[ j + 1]:
+        for j in range(n - i - 1):
+            if array[j] > array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
 
-                temp = array[j]
-                array[j] = array[j + 1]
-                array[j + 1] = temp
-    print(f"Ascend Order: {array}")
+                already_sorted = False
 
+        if already_sorted:
+            break
+    print(f"Ascending Order: {array}")
 
 def descend_sort(array):
-    # loop to access each array element
-    for i in range(len(array)):
-        # loop to compare array elements
-        for j in range(0, len(array) - i - 1):
+    n = len(array)
 
-            # compare two adjacent elements
-            # change < to sort in descending order
-            if array[j] > array[j + 1]:
-                temp = array[j]
-                array[j] = array[j + 1]
-                array[j + 1] = temp
-    print(f"Descend Order: {array}")
+    for i in range(n):
+        already_sorted = True
+
+        for j in range(n - i - 1):
+            if array[j] < array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
+
+                already_sorted = False
+
+        if already_sorted:
+            break
+    print(f"Descending Order: {array}")
 
 def main():
     user_input = input("Please enter a list a numbers separated by a space: ").split()
